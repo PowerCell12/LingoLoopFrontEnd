975160713cb4b34ab6d4393aeed1192206e6baaa
PowerCell12 <gumbal4e@gmail.com>
2025-06-17 21:30:52 +0300

    Pre Angular 20 update state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: "Home"
+    },
+    {
+        path: "register",
+        loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent),
+        title: "Register"
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent),
+        title: "Not-Found",
+    },
+];
+
